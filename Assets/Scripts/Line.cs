bb95cedd42f0006dfdc91cf098820206bda6f7e7
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2016-12-06 16:37:41 +0100

    added base assets and maze generation

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Line {
+    public Vector2 Start { get; set; }
+    public Vector2 End { get; set; }
+
+    public Line(float x1, float y1, float x2, float y2)
+    {
+        Start = new Vector2(x1, y1);
+        End = new Vector2(x2, y2);
+    }
+}
---
+++ Assets/Scripts/MazeGeneratorBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class MazeGeneratorBehaviour : MonoBehaviour
+{
+
+    private MazeGenerator mz;
+    private char[,] maze;
+
+    public int StageWidth = 51;
+    public int StageHeight = 51;
+    public int NumberRoomTries = 30;
+    public int ExtraConnectorChance = 20;
+    public bool RemoveDeadEnds = true;
+
+    public GameObject Wall;
+    public GameObject Floor;
+    public GameObject Door;
+
+    public GameObject Player;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (StageWidth % 2 == 0)
+            StageWidth++;
+        if (StageHeight % 2 == 0)
+            StageHeight++;
+
+        mz = new MazeGenerator(StageWidth, StageHeight, NumberRoomTries, ExtraConnectorChance, RemoveDeadEnds);
+        maze = mz.Generate();
+        mz.PrintDebug();
+
+        for (int y = 0; y < maze.GetLength(1); ++y)
+        {
+            for (int x = 0; x < maze.GetLength(0); ++x)
+            {
+                GameObject whatToSpawn = new GameObject();
+                float size = Wall.transform.localScale.x;
+                bool spawnFloor = false;
+                if (maze[x, y] == Tiles.wall)
+                {
+                    whatToSpawn = Wall;
+                }
+                else if (maze[x, y] == Tiles.floor)
+                {
+                    whatToSpawn = Floor;
+                }
+                else if (maze[x, y] == Tiles.door)
+                {
+                    whatToSpawn = Door;
+                }
+                else if (maze[x, y] == Tiles.player)
+                {
+                    whatToSpawn = Player;
+                    spawnFloor = true;
+                }
+                Instantiate(whatToSpawn, new Vector3(x * size, !spawnFloor ? size * (-1) : 0, y * size), new Quaternion());
+                if (spawnFloor) Instantiate(Floor, new Vector3(x * size, size * (-1), y * size), new Quaternion());
+            }
+        }
+    }
+}
